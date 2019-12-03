cf2e6da7d77c2530b9c4412b89d4289d6e29749d
romanilicev <shiftr123>
2019-12-04 00:34:27 +0300

    Адаптация с помощью класса Dimensions

--- app/src/screens/TodoScreen.js
+++ app/src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {StyleSheet, View, Button} from 'react-native'
+import {StyleSheet, View, Dimensions} from 'react-native'
 import {THEME} from '../theme'
 import {AppCard} from "../components/ui/AppCard";
 import {EditModal} from "../components/EditModal";
@@ -51,7 +51,8 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     button: {
-        width: '40%'
+        // width: Dimensions.get('window').width / 3
+        width: Dimensions.get('window').width > 400 ? 150 : 100
     },
     card: {
         marginBottom: 20,
